a57bc1cca91ec4d560d2e2752cb382fbc5f9d7dd
bilunabeel <bilunabeelmv@gmail.com>
2023-11-12 19:05:24 +0530

    nav bug fix

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -11,16 +11,16 @@ const Nav = () => {
     <nav className='max-md:bg-white bg-none md:h-20 w-full flex items-center justify-between max-md:flex-col max-md:gap-9 max-md:py-5  fixed padding-x font-jost' >
         <img href='#home' src="bilu_logo.png" className=' h-10' alt="" />
         <div onClick={()=>setOpen(!open)} className=' md:hidden  absolute right-16'>
-        <FontAwesomeIcon size="2x" icon={open ? faBars:faXmark} />
+        <FontAwesomeIcon size="2x" icon={open ? faXmark:faBars} />
 
         </div>
-        <li className={` flex gap-9 max-md:flex-col max-md:items-center max-md:gap-9 text-xl font-regular ${open && `max-md:hidden`}`} >
+        <li className={` flex gap-9 max-md:flex-col max-md:items-center max-md:gap-9 text-xl font-regular ${!open && `max-md:hidden`}`} >
             <ul href='#home' className='cursor-pointer'>Home</ul>
             <ul href='#services' className='cursor-pointer' >Services</ul>
             <ul href='#portfolios' className='cursor-pointer' >Portfolios</ul>
             <ul href='#testimonials' className='cursor-pointer' >Testimonials</ul>
         </li>
-        <div className={`bg-yellow_primary px-10 py-2 rounded-full text-xl font-regular ${open && `max-md:hidden`}`}>Let's Talk</div>
+        <div className={`bg-yellow_primary px-10 py-2 rounded-full text-xl font-regular ${!open && `max-md:hidden`}`}>Let's Talk</div>
     </nav>
   )
 }
