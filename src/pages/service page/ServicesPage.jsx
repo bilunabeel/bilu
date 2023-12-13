0e831c536f979a2626454b19abb93bead14f09a1
bilunabeel <bilunabeelmv@gmail.com>
2023-12-13 23:23:33 +0530

    image fix

--- src/pages/service page/ServicesPage.jsx
+++ src/pages/service page/ServicesPage.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 
 const images = [
-  {name:'img1',src:"/masonryImages/img1.jpeg"},
-  {name:'img2',src:"/masonryImages/img2.jpeg"},
-  {name:'img3',src:"/masonryImages/img3.jpeg"},
-  {name:'img4',src:"/masonryImages/img4.jpeg"},
-  {name:'img5',src:"/masonryImages/img5.jpeg"},
-  {name:'img6',src:"/masonryImages/img6.jpeg"},
-  {name:'img7',src:"/masonryImages/img7.jpeg"},
-  {name:'img8',src:"/masonryImages/img8.jpeg"},
-  {name:'img9',src:"/masonryImages/img9.jpeg"},
-  {name:'img10',src:"/masonryImages/img10.jpeg"},
-  {name:'img11',src:"/masonryImages/img11.jpeg"},
-  {name:'img12',src:"/masonryImages/img12.jpeg"}
+  {name:'img1',src:"masonryImages/img1.jpeg"},
+  {name:'img2',src:"masonryImages/img2.jpeg"},
+  {name:'img3',src:"masonryImages/img3.jpeg"},
+  {name:'img4',src:"masonryImages/img4.jpeg"},
+  {name:'img5',src:"masonryImages/img5.jpeg"},
+  {name:'img6',src:"masonryImages/img6.jpeg"},
+  {name:'img7',src:"masonryImages/img7.jpeg"},
+  {name:'img8',src:"masonryImages/img8.jpeg"},
+  {name:'img9',src:"masonryImages/img9.jpeg"},
+  {name:'img10',src:"masonryImages/img10.jpeg"},
+  {name:'img11',src:"masonryImages/img11.jpeg"},
+  {name:'img12',src:"masonryImages/img12.jpeg"}
 ]
 
 const ServicesPage = () => {
